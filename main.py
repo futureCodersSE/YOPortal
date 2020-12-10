9f0d2edc4aaa657988139717b5a31d34506e7d68
futureCoders <karen@futurecoders.org.uk>
2020-12-10 21:54:18 +0000

    Moved get_YO_user_info and get_emotion_image to helpers.py.  Refactored image names to remove _

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ from yolist import list_yos
 # print(get_data("supporter"))
 
 # supporterid, supportername = login_supporter()
-# supportername = "Boris"
-# supporterid = 1
-# list_yos(supporterid, supportername)
+supportername = "Boris"
+supporterid = 1
+list_yos(supporterid, supportername)
 
 # ADD CHALLENGE CODE HERE
 
@@ -37,30 +37,7 @@ def get_YO_user_info(userid):
       
   return name, surname, email, logged_in
 
-name, surname, email, logged_in = get_YO_user_info(3)
-print(name, surname, email, logged_in)
+# name, surname, email, logged_in = get_YO_user_info(3)
+# print(name, surname, email, logged_in)
 
-def get_emotion_image(emotion):
-  if emotion == 1:
-    return "images/very_sad.png"
-  elif emotion == 2:
-    return "images/slightly_sad.png"
-  elif emotion == 3:
-    return "images/ok.png"
-  elif emotion == 4:
-    return "images/happy.png"
-  elif emotion == 5:
-    return "images/very_happy.png"
-  else:
-    return "Enter number between 1 and 5"
-emotion1 = get_emotion_image (5)
-emotion2 = get_emotion_image (4)
-emotion3 = get_emotion_image (3)
-emotion4 = get_emotion_image (2)
-emotion5 = get_emotion_image (1)
 
-print(emotion1)
-print(emotion2)
-print(emotion3)
-print(emotion4)
-print(emotion5)
--- yolist.py
+++ yolist.py
@@ -1,10 +1,8 @@
 import PySimpleGUI as sg
 from datahandlers import get_data
+from helpers import get_YO_user_info, get_emotion_image
 
-def get_emotion_image(emotion):
-  image_file = ""
-  # return the correct image for the emotion
-  return image_file
+
 
 def list_yos(supporterid, supportername):
   # get list of all yos with this supporter id
@@ -13,10 +11,11 @@ def list_yos(supporterid, supportername):
       sg.Text('YOs for supporter: ', font=('Helvetica', 12, 'bold')),
       sg.Text(supportername,font=('Helvetica', 12, 'bold'))],
     [
-      sg.Text('Name',size=(10,2),font=('Helvetica', 10, 'bold')),
-      sg.Text('Surname',size=(10,2),font=('Helvetica', 10, 'bold')),
-      sg.Text('Start date',size=(10,2),font=('Helvetica', 10, 'bold')),
-      sg.Text('Emotion status',size=(10,2),font=('Helvetica', 10, 'bold'))
+      sg.Text('Name',size=(14,1),font=('Helvetica', 10, 'bold')),
+      sg.Text('Surname',size=(14,1),font=('Helvetica', 10, 'bold')),
+      sg.Text('Start date',size=(14,1),font=('Helvetica', 10, 'bold')),
+      sg.Text('Emotion',size=(14,1),font=('Helvetica', 10, 'bold')),
+      sg.Text('Messages', size=(14,1),font=('Helvetica',10,'bold'))
     ]
   ]
   yos = get_data("yos")
@@ -24,18 +23,18 @@ def list_yos(supporterid, supportername):
   for yo in yos:
     for user in users:
       if user['ID'] == yo['USERID']:
-        name = user['NAME']
-        surname = user['SURNAME']
+        name, surname, email, logged_in = get_YO_user_info(user['ID'])
         start_date = yo['STARTDATE']
         emotion_status = yo['EMOTION']
         layout.append([
-          sg.Text(name, size=(10,2)),
-          sg.Text(surname, size=(10,2)),
-          sg.Text(start_date, size=(10,2)),
-          sg.Button(button_color=("red","red"), image_filename=get_emotion_image(emotion_status),image_size=(20,20)), 
+          sg.Text(name, size=(16,1)),
+          sg.Text(surname, size=(16,1)),
+          sg.Text(start_date, size=(18,1)),
+          sg.Button(button_color=("gray","gray"), image_filename=get_emotion_image(emotion_status),image_size=(20,20)),
+          sg.Text("", size=(24,1)), 
           sg.Button('Select')])
   layout.append([sg.Button('Close')])
-  window = sg.Window('YOs').Layout(layout)
+  window = sg.Window('YOs', size=(800,400)).Layout(layout)
 
   # wait for a button to be clicked and handle it
   while True:
