42367c47c02cc0a6c049ebef6feab6f917a2d1d5
futureCoders <karen@futurecoders.org.uk>
2021-01-07 08:46:45 +0000

    Added Dee's update function to main code base

--- datahandlers.py
+++ datahandlers.py
@@ -21,3 +21,24 @@ def get_data(tablename):
               index += 1
             user_list.append(user_data)
   return user_list
+
+# given an edited set of the table data, replace the existing data with the edited set (edited set may have records changed, added or deleted.)
+# Dee Thrussell
+def update_data(tablename,data_list):
+  filename = 'data/yop-' + tablename + '.csv'
+  headings = []
+  with open(filename) as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    for row in csv_reader:
+      headings = row
+      break
+  with open(filename,'w') as csv_file:
+    csv_writer = csv.writer(csv_file, delimiter=',')
+    csv_writer.writerow(headings)
+    for data in data_list:
+      new_data = []
+      for heading in headings:     
+        new_data.append(data[heading])
+      print(new_data)
+      csv_writer.writerow(new_data)
+  return data_list
